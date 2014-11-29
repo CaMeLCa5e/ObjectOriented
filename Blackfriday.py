3c3bb6a8bdda8c4140b3e8da2b4e98454c618926
Jack Martin <JM273606@gmail.com>
2014-11-28 23:06:43 -0500

    updates

---
+++ Blackfriday.py
@@ -0,0 +1,22 @@
+#Blackfriday shopping
+
+class shopping:
+	televisions = "Sale!"
+	dishwashers = "50 percent off!"
+	sweaters = "full price"
+	dishes = "full price"
+	ps3 = "100$"
+	def shoppingMethod(self):
+		return "it worked!"
+
+shoppingTrip = shopping 
+
+print shopping
+print shopping.televisions
+print shopping.sweaters
+print shopping.dishes
+
+
+
+
+
---
+++ oop 11.28.14.py
@@ -0,0 +1,16 @@
+class exampleClass: 
+	eyes = "green"
+	height = "tall"
+	age = 27
+	def thisMethod(self):
+		return "hey, it worked!"
+
+
+
+exampleObject = exampleClass()
+
+print exampleClass
+print exampleObject.eyes
+print exampleObject.height
+
+print exampleObject.thisMethod()
